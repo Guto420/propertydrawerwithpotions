732a96f8d69fc6773ea5d752d421d57a8bb74f2d
Gustavo <Amaralgustavo59@gmail.com>
2017-09-24 18:12:37 -0300

    24.09.2017

---
+++ 3003-b3-av3-master/Assets/Editor/Potiondrawer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomPropertyDrawer(typeof(PotionSelectorAttribute))]
+public class Potiondrawer : PropertyDrawer {
+
+	private Potion[] potionaux;
+	int index;
+
+	public override void OnGUI (Rect position, SerializedProperty property, GUIContent label)
+	{
+		index = 0;
+		potionaux = PotionDB.Instance.potions;
+		string[] nomesdaspocoes = new string[potionaux.Length];
+
+		for (int u = 0; u < potionaux.Length; u++) 
+		{
+			nomesdaspocoes [u] = potionaux [u].name;
+
+		}
+
+		for (int i = 0; i < potionaux.Length; i++) 
+		{
+				if (nomesdaspocoes [i] == property.stringValue) 
+				{
+					index = i;
+					break;
+				}
+		}
+
+		index = EditorGUI.Popup(position, label.text, index, nomesdaspocoes);
+
+	}
+
+
+}
---
+++ 3003-b3-av3-master/Assets/Scripts/TestBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[System.AttributeUsage(System.AttributeTargets.Field)]
+public class PotionSelectorAttribute : PropertyAttribute
+{
+	
+}
+public class TestBehaviour : MonoBehaviour
+{
+	[PotionSelector]
+	public string potionName;
+
+	public void Start()
+	{
+		// PotionDB.Instance.potions pra acessar o "banco de dados" de pocoes
+		// Imprimir o quanto aquela potion recupera
+	}
+}
